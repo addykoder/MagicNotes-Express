9e4bdb37ce4d216e192f3aed12a27e8a0b181683
Aditya Tripathi <warphold@gmail.com>
2026-04-09 22:43:54 +0530

    Update MongoDB connection string with new credentials

--- backend/js/mongoose.js
+++ backend/js/mongoose.js
@@ -5,7 +5,7 @@ params = {
 	useUnifiedTopology: true,
 };
 mongoose.connect(
-	'mongodb+srv://androplayboy:<password>@cluster0.lfawf.mongodb.net/notes',
+	'mongodb+srv://jeevandhara:JD1234@jeevandharac0.yrzcqc7.mongodb.net//notes',
 	params
 );
 
